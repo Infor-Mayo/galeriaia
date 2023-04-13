00bbbbfc40ebd01115a1369fca78983a0dc13fc4
Infor_Mayo <106414650+Infor-Mayo@users.noreply.github.com>
2023-04-12 23:29:15 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -5,31 +5,6 @@ const carousel = document.getElementById("carousel");
 let currentIndex = 0;
 
 function fetchImages() {
-  if ("caches" in window) {
-    caches.match(GITHUB_FOLDER_URL)
-      .then(response => {
-        if (response) {
-          response.json().then(data => {
-            const imageUrls = data.filter(file => {
-              const extension = file.name.split(".").pop().toLowerCase();
-              return IMAGE_FILE_EXTENSIONS.includes(extension);
-            }).map(file => file.download_url);
-            createCarousel(imageUrls);
-          });
-        } else {
-          fetchImagesFromAPI();
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        fetchImagesFromAPI();
-      });
-  } else {
-    fetchImagesFromAPI();
-  }
-}
-
-function fetchImagesFromAPI() {
   fetch(GITHUB_FOLDER_URL)
     .then(response => response.json())
     .then(data => {
@@ -38,19 +13,8 @@ function fetchImagesFromAPI() {
         return IMAGE_FILE_EXTENSIONS.includes(extension);
       }).map(file => file.download_url);
       createCarousel(imageUrls);
-      cacheImages(imageUrls);
     })
     .catch(error => console.error(error));
-}
-
-function cacheImages(imageUrls) {
-  if ("caches" in window) {
-    caches.open("github-carousel").then(cache => {
-      imageUrls.forEach(url => {
-        cache.add(url);
-      });
-    });
-  }
 }
 
 function createCarousel(imageUrls) {
